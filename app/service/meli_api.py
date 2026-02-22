501b00d529e66419a45319057244741c9a76df11
nicolasgallu <nicolasgallu@pm.me>
2026-02-22 17:50:27 -0300

    foff

--- app/service/meli_api.py
+++ app/service/meli_api.py
@@ -112,6 +112,7 @@ def publish_item(item_data, public_images, token):
 def update_item(item_data, public_images, token):
     """Update MercadoLibre item"""
     meli_id = item_data['meli_id']
+    logger.info(f"the meli id is: {meli_id}")
     if meli_id is None or meli_id == '':
         logger.error(f"Item: {item_data['item_id']} doesnt exists in mercadolibre, nothing to update.")
         return
