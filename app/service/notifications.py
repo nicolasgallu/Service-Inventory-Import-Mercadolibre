4d5e027361c74aa3ca96e732d0cc4fe8962f437c
nicolasgallu <nicolasgallu@pm.me>
2026-03-01 23:57:52 -0300

    sumando tests

--- app/service/notifications.py
+++ app/service/notifications.py
@@ -1,20 +1,22 @@
 import requests
 
 def enviar_mensaje_whapi(token, telefono, mensaje):
-    if isinstance(telefono,int):
-
-        url = "https://gate.whapi.cloud/messages/text"
-        payload = {
-            "to": f"{telefono}",
-            "body": mensaje
-        }
-        headers = {
-            "accept": "application/json",
-            "content-type": "application/json",
-            "authorization": f"Bearer {token}"
-        }
-
-        res = requests.post(url, json=payload, headers=headers)
-        return res.json()
-    else:
+    if mensaje is None or mensaje == "":
+        raise ValueError
+    if isinstance(telefono, int) == False:
         raise TypeError
+    
+    url = "https://gate.whapi.cloud/messages/text"
+    payload = {
+        "to": f"{telefono}",
+        "body": mensaje
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "authorization": f"Bearer {token}"
+    }
+    res = requests.post(url, json=payload, headers=headers)
+    return res.json()
+
+    
---
+++ tests/test_notifications.py
@@ -0,0 +1,10 @@
+from app.service.notifications import enviar_mensaje_whapi
+import pytest
+
+def test_bad_phone_type():
+    with pytest.raises(TypeError) as excinfo:
+        assert enviar_mensaje_whapi("token", "123456", "mensaje")
+
+def test_empty_message():
+    with pytest.raises(ValueError) as excinfo:
+        assert enviar_mensaje_whapi("token", "123456", None)
