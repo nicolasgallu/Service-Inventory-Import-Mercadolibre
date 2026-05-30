ba09a6f322cf8095d7196a5618352cc2e8c28afe
nicolasgallu <nicolasgallu@pm.me>
2026-05-30 14:45:15 -0300

    fixing error on calculator

--- app/service/pipe_calculator.py
+++ app/service/pipe_calculator.py
@@ -1,5 +1,5 @@
 from app.service.secrets import meli_secrets
-from app.service.meli_api import get_data_for_meli, _generate_category_id, calculate_cost
+from app.service.meli_api import get_data_for_meli, _generate_category_options, calculate_cost
 from app.utils.logger import logger
 
 def calculating_cost(response):
@@ -9,7 +9,7 @@ def calculating_cost(response):
     user_id = token.split('-')[-1]
 
     item_data = get_data_for_meli(item_id)
-    if item_data.get('category_id'):
-        _generate_category_id(item_id, item_data.get('product_name'), token)
+    if item_data.get('category_options') == None:
+        _generate_category_options(item_id, item_data.get('product_name'), token)
 
     calculate_cost(item_data, user_id, token)
